07900cf569c06791f98db952f2162203226beac2
Ian G <yo.ian.g@gmail.com>
2015-06-09 15:47:06 -0400

    bug fix

--- StoryboardInfo/NavigationControllerClassInfo.swift
+++ StoryboardInfo/NavigationControllerClassInfo.swift
@@ -18,6 +18,6 @@ public class NavigationControllerClassInfo: ViewControllerClassInfo {
         } else {
             useClassName = NavigationControllerClassInfo.defaultClass
         }
-        super.init(className: className)
+        super.init(className: useClassName)
     }
 }
