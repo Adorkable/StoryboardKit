059e0e53db725d84c3213b34ecb246267a1dd75c
Ian G <yo.ian.g@gmail.com>
2015-06-29 10:17:35 -0400

    File organization

--- StoryboardKit/Utility.swift
+++ StoryboardKit/Utility.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// TODO: organize appropriately, evaulate need of each
+
 // http://stackoverflow.com/a/24128121/96153
 public class StoryboardKit_WeakWrapper<T: AnyObject> {
     weak var value : T?
@@ -19,4 +21,26 @@ public class StoryboardKit_WeakWrapper<T: AnyObject> {
 public enum StoryboardKit_Either<TLeft : AnyObject, TRight : AnyObject> {
     case Left(TLeft)
     case Right(TRight)
+}
+
+protocol Idable {
+    var id : String { get }
+}
+
+func objectsWithId<T : Idable>(id : String, objects : [T]) -> [T] {
+    return objects.filter({ $0.id == id })
+}
+
+func firstObject<T>(objects : [T]) -> T? {
+    var result : T?
+    
+    if objects.count > 0 {
+        result = objects[0]
+    }
+    
+    return result
+}
+
+func firstObjectWithId<T : Idable>(id : String, objects : [T]) -> T? {
+    return firstObject( objectsWithId(id, objects) )
 }
